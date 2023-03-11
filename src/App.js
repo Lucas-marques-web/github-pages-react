af66462463291f3715805742dc4780ff6583a66a
unknown <lp411301@gmail.com>
2023-03-11 14:32:25 -0300

    change message

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import testImg from './lucas.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <img src={testImg}></img>
+        <h1>
+          My code works good
+        </h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
